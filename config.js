a51b10c0694810c0dc487ccd87632c6665aa6465
Daniel Montes de Oca <danielmontesdeoca@Daniels-MacBook-Pro.local>
2014-11-02 00:10:13 -0700

    add kue package dependency and configuration

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
 module.exports = {
   port: 3300,
-  db:{
+  db: {
     url: 'mongodb://localhost:27017/tripthru', 
     user:'', 
     password:''
+  },
+  kue: {
+    prefix: 'q',
+    redis: {
+      port: 6379,
+      host: '10.0.50.20',
+      auth: ''
+    }
   }
 }
